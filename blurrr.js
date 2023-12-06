9eb9a2a84e098c27da6f78ea7ebc97b54acd108d
urfan1 <64177520+urfan1@users.noreply.github.com>
2023-12-06 20:48:39 +0800

    Update blurrr.js

--- blurrr.js
+++ blurrr.js
@@ -6,13 +6,7 @@
 > 更新時間：2022-07-24
 > 特别說明：本腳本僅供學習交流使用，禁止轉載售賣
  
-重写添加 
-
-Url   ^https://video-beats-api.360in.com/v1/user/vip
-类型   script-response-body 
-脚本   *****.js
-
-（js脚本放在手机本地圈x目录下的Scripts目录里面   文件重命名英文字母）
+***********************
 
 [rewrite_local]
 
@@ -20,7 +14,7 @@ Url   ^https://video-beats-api.360in.com/v1/user/vip
 
 [mitm] 
 
-hostname=video-beats-api.360in.com
+hostname = video-beats-api.360in.com
 
 ***********************************/
 
